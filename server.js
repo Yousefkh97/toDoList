2ed38e983fb5027c31e3fbb3ced1a31858992adf
Yousef Khatib <yousefkh97@gmail.com>
2020-08-17 13:39:37 +0300

    update

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => { console.log("App is Listening on port", port) })
