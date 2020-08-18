b51745a6761871c15f38cb93261730e515d5107b
Lina <nijemlina@gmail.com>
2020-08-18 09:27:58 +0300

    todo

--- client/src/view/components/Home/Home.js
+++ client/src/view/components/Home/Home.js
@@ -15,6 +15,7 @@ function Home(props) {
                 To Do List
                 <Link to="/login"><button>Log In</button> </Link>
                 <Link to="/Register"><button>Register now</button></Link>
+                <Link to="/ToDo"><button>ToDO</button></Link>
             </div>
 
             <div className="logo">
