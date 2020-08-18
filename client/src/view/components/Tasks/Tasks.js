aab9df0e3d3656add5fad39119d1e2570a4cedf8
Lina <nijemlina@gmail.com>
2020-08-18 20:29:44 +0300

    ..

--- client/src/view/components/Tasks/Tasks.js
+++ client/src/view/components/Tasks/Tasks.js
@@ -24,12 +24,48 @@ function Tasks(props) {
                 }
                 <div className="newTodo">
                     <div><label>Add new ToDo</label></div>
-                    <div><input type="text" name="newTodo" placeholder="Title: " /></div>
-                    <div><input type="text" name="newTodo" placeholder="Content: " /></div>
-                    <button>+</button>
+                    <div><input type="text" id="newTitle" placeholder="Title: " /></div>
+                    <div><input type="text" id="newContent" placeholder="Content: " /></div>
+                    <button type="submit" onclick="AddNewToDo(event)">+</button>
                 </div>
             </form>
         </div>
     )
 }
 export default Tasks;
+
+
+
+function AddNewToDo(e)
+{
+    e.preventDefault();
+    try 
+    {
+        const newTitle = e.target.elements.newTitle.value;
+        const newContent = e.target.elements.newContent.value;
+
+        // user: {
+        //     userEmail: String,
+        //     firstName: String,
+        //     lastName: String,
+        //     imgUrl: String,
+        //     password: String
+        // },
+        // taskTitle: String,
+        // taskContent: String,
+        // done: Boolean
+
+
+        fetch('/addNewToDo', {
+            method: 'PUT',
+            body: JSON.stringify(userItem),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            })
+
+    }catch(err) 
+    {
+      console.error(err)
+    }
+}
