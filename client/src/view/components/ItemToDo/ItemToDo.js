4d6382f8487c19573d81512c43b2aad63d1e54e9
Lina <nijemlina@gmail.com>
2020-08-18 14:52:50 +0300

    update check

--- client/src/view/components/ItemToDo/ItemToDo.js
+++ client/src/view/components/ItemToDo/ItemToDo.js
@@ -3,13 +3,13 @@ import './ItemToDo.css';
 
 function ItemToDo(props) {
     const { ItemToDo } = props;
-    const [isCheck, setisCheck] = useState(false);
+    const [isCheck, setIsCheck] = useState(true);
 
 
         return (
-            <div className="todo">
+            <div className={isCheck?'todo':"todo checked"}>
                 <div className="item" id='taskTitle'> {ItemToDo.taskTitle}</div>
-                <div className="item"><input type="checkbox" id="done" name="done" value="done" /></div>
+                <div className="item"><input type="checkbox" name="done"  onChange={()=>{setIsCheck(!isCheck)}}/></div>
                 <div className="item"><button>-</button></div>
             </div>
         )
