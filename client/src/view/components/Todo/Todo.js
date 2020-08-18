20f68a34f528ac50008f9eefd797a7875ce3d4f3
Lina <nijemlina@gmail.com>
2020-08-18 10:28:55 +0300

    todo

--- client/src/view/components/Todo/Todo.js
+++ client/src/view/components/Todo/Todo.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './Todo.css';
+import add from '../../../img/add.png'
+import deleteIcon from '../../../img/delete.png'
 
 function Todo(props) {
     return (
         <div className="headerTodo">
             <p id='title'>Nijem ToDO List</p>
             {/* map */}
-            <p>play lol</p><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+
             <div className="formTodo">
                 <form>
-                    <div>Lina</div>
-                    
-                    <input type="text" name="newTodo" placeholder="Add new Todo" />
-                    <button></button>
+                    <p id='name'>Lina</p>
+                    <div className="todo">
+                        <div className="item">play lol</div>
+                        <div className="item"><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" /></div>
+                        <div className="item"><button><img src={deleteIcon} width='15px' height='15px' /></button></div>
+                    </div>
+                    <div className="todo">
+                        <div className="item">play lol</div>
+                        <div className="item"><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" /></div>
+                        <div className="item"><button><img src={deleteIcon} width='15px' height='15px' /></button></div>
+                    </div>
+                    <div className="todo">
+                        <div className="item">play lol</div>
+                        <div className="item"><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" /></div>
+                        <div className="item"><button><img src={deleteIcon} width='15px' height='15px' /></button></div>
+                    </div>
+                    <div className="newTodo">
+                        <input type="text" name="newTodo" placeholder="Add new Todo" />
+                        <button>+</button>
+                        {/* <img src={add} width="15px" height="15px" /> */}
+                    </div>
+
                 </form>
             </div>
         </div>
